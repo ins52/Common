38feb4beaa41d447f6289ee1a37d77e4ffa23179
aignatiev <aignatiev@Mac-mini.local>
2013-04-04 17:46:13 +0400

    Initial Commit

---
+++ Common/Common.h
@@ -0,0 +1,13 @@
+//
+//  Common.h
+//  Common
+//
+//  Created by aignatiev on 04.04.13.
+//  Copyright (c) 2013 aignatiev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Common : NSObject
+
+@end
---
+++ Common/Common.m
@@ -0,0 +1,13 @@
+//
+//  Common.m
+//  Common
+//
+//  Created by aignatiev on 04.04.13.
+//  Copyright (c) 2013 aignatiev. All rights reserved.
+//
+
+#import "Common.h"
+
+@implementation Common
+
+@end
